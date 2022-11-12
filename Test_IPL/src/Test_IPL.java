d0174715baec33ff98b073d08ddd0c4f6fd0940d
Shubh Bhatnagar <70750865+S-h-u-b-h27@users.noreply.github.com>
2022-11-12 16:44:36 +0530

    Update Test_IPL.java

--- Test_IPL/src/Test_IPL.java
+++ Test_IPL/src/Test_IPL.java
@@ -5,21 +5,33 @@ class Pair{
 public class Test_IPL {
 
 	public static void main(String[] args) {
-		String[][] IPL_leaderboard = {
-				{"GT","20","Win","Win","Loss","Loss","Win"},
-				{"LSG","18","Win","Loss","Loss","Win","Win"},
-				{"RR","16","Win","Loss","Win","Loss","Loss"},
-				{"DC","14","Win","Win","Loss","Win","Loss"},
-				{"RCB","14","Loss","Win","Win","Loss","Loss"},
-				{"KKR","12","Loss","Win","Win","Loss","Win"},
-				{"PBKS","12","Loss","Win","Loss","Win","Loss"},
-				{"SRH","12","Win","Loss","Loss","Loss","Loss"},
-				{"CSK","8","Loss","Loss","Win","Loss","Win"},
-				{"MI","6","Loss","Win","Loss","Win","Win"},
-				};
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number of teams");
+		int number_of_teams=Integer.parseInt(sc.nextLine());
+		String IPL_leaderboard[][]=new String[number_of_teams][]; 
+		for(int itr_teams=0;itr_teams<number_of_teams;itr_teams++)
+		{
+			System.out.println("Enter the details of team "+(itr_teams+1)+" result :");
+			System.out.println("Example- (GT 20 Win Win Loss Loss Win)");
+			System.out.print("->");
+			String dataof_team=sc.nextLine();
+			IPL_leaderboard[itr_teams]=(dataof_team.trim()).split(" ");
+		}
+//		String[][] IPL_leaderboard = {
+//				{"GT","20","Win","Win","Loss","Loss","Win"},
+//				{"LSG","18","Win","Loss","Loss","Win","Win"},
+//				{"RR","16","Win","Loss","Win","Loss","Loss"},
+//				{"DC","14","Win","Win","Loss","Win","Loss"},
+//				{"RCB","14","Loss","Win","Win","Loss","Loss"},
+//				{"KKR","12","Loss","Win","Win","Loss","Win"},
+//				{"PBKS","12","Loss","Win","Loss","Win","Loss"},
+//				{"SRH","12","Win","Loss","Loss","Loss","Loss"},
+//				{"CSK","8","Loss","Loss","Win","Loss","Win"},
+//				{"MI","6","Loss","Win","Loss","Win","Win"},
+//				};
 		
 		
-		for(int teamRank=0;teamRank<10;teamRank++)
+		for(int teamRank=0;teamRank<number_of_teams;teamRank++)
 		{
 			
 			for(int coloumn=0;coloumn<7;coloumn++)
@@ -31,16 +43,16 @@ public class Test_IPL {
 		
 		
 		HashMap<String,Integer> teamWith2_consi_losses=new HashMap<String,Integer>();
-		String [][] filtered_team=new String[10][4];
-		for(int teamRank=0;teamRank<10;teamRank++)
+		for(int teamRank=0;teamRank<number_of_teams;teamRank++)
 		{
 			String prev="";
 			
 			for(int coloumn=2;coloumn<7;coloumn++)
 			{
-				if(IPL_leaderboard[teamRank][coloumn]=="Loss")
+				//System.out.println(IPL_leaderboard[teamRank][coloumn]);
+				if((IPL_leaderboard[teamRank][coloumn].trim()).equals("Loss"))
 				{
-					if(prev==IPL_leaderboard[teamRank][coloumn])
+					if(prev.equals(IPL_leaderboard[teamRank][coloumn]))
 					{
 						teamWith2_consi_losses.put(IPL_leaderboard[teamRank][0],Integer.parseInt(IPL_leaderboard[teamRank][1]));
 					}
@@ -58,7 +70,7 @@ public class Test_IPL {
 		System.out.println();
 		
 		
-		for(int teamRank=0;teamRank<10;teamRank++)
+		for(int teamRank=0;teamRank<number_of_teams;teamRank++)
 		{
 			String prev="";
 			int win_count=0;
@@ -70,9 +82,10 @@ public class Test_IPL {
 				for(int coloumn=2;coloumn<7;coloumn++)
 				{
 					
-					if(IPL_leaderboard[teamRank][coloumn]=="Loss")
+					if(IPL_leaderboard[teamRank][coloumn].equals("Loss"))
 					{
-						if(prev==IPL_leaderboard[teamRank][coloumn])
+						
+						if(prev.equals(IPL_leaderboard[teamRank][coloumn]))
 						{
 							loss_count++;
 						}
@@ -84,7 +97,7 @@ public class Test_IPL {
 					}
 					else
 					{
-						if(prev==IPL_leaderboard[teamRank][coloumn])
+						if(prev.equals(IPL_leaderboard[teamRank][coloumn]))
 						{
 							win_count++;
 						}
@@ -116,8 +129,12 @@ public class Test_IPL {
 		{
 			sumOfPoints=sumOfPoints+teamWith2_consi_losses.get(filtered_teams);
 		}
+		int average_of_points=0;
+		if(teamWith2_consi_losses.size()>0)
+		{
+			average_of_points=sumOfPoints/teamWith2_consi_losses.size();
+		}
 		
-		int average_of_points=sumOfPoints/teamWith2_consi_losses.size();
 		System.out.println();
 		System.out.println();
 		System.out.println();
